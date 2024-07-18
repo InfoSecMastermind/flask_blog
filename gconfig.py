3fe110b8dd7bb4fc41d2df59889e2a601f983a34
InfoSecMastermind <muhammadalinafeespro@gmail.com>
2024-07-18 14:03:17 +0000

    Added the Dockerfiles, Compose files and database init commands

---
+++ gconfig.py
@@ -0,0 +1,6 @@
+# gconfig.py
+bind = '127.0.0.1:5000'
+workers = 3 
+loglevel = 'info'
+errorlog = '/home/ubuntu/do_project/gunicorn/error.log'
+accesslog = '/home/ubuntu/do_project/gunicorn/access.log'
